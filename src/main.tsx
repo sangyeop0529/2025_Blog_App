9f950edfee73d155e28ed4946656e2c8f4bcb3e3
DELL <sangyeop0529@gmail.com>
2025-06-13 16:45:58 +0900

    home.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "the-new-css-reset/css/reset.css";
+
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <div>
+          <Link to="/posts/new">글쓰기</Link>
+          <Link to="/posts">게시글</Link>
+          <Link to="/profile">프로필</Link>
+        </div>
+      </header>
+      <div className="post-list">post list</div>
+      <footer>
+        <div>Menu 1</div>
+        <div>Menu 2</div>
+        <div>Menu 3</div>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
