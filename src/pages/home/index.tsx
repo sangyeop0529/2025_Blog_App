db46e65099cf61be630a7d5fc9fc511e7134a5ba
DELL <sangyeop0529@gmail.com>
2025-06-14 16:18:54 +0900

    carousel 컴포넌트 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import PostList from "../../components/PostList";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import PostList from "components/PostList";
+import Carousel from "components/Carousel";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Carousel />
       <PostList />
       <Footer />
     </div>
